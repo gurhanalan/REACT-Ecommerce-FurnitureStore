b5c12f29c5659b4e6d708dff336d18bcccaed2cc
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-13 01:44:47 +0300

    Adding stars comp to SingleProductPage

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -66,7 +66,8 @@ const SingleProductPage = () => {
                     <ProductImages images={images} />
                     <section className="content">
                         <h2>{name}</h2>
-                        <Stars />
+                        {/* <Stars stars={stars} reviews={reviews} /> */}
+                        <Stars {...{ stars, reviews }} />
                         <h5 className="price">{formatPrice(price)}</h5>
                         <p className="desc">{description}</p>
                         <p className="info">
