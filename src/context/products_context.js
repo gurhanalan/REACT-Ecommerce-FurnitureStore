2472e993cc4240061f7a6523fb16457cdd216368
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-12 23:33:41 +0300

    Add single product fetch & loading n error page

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
     products_error: false,
     products: [],
     featured_products: [],
+    single_product_loading: false,
+    single_product_error: false,
+    single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -43,13 +46,23 @@ export const ProductsProvider = ({ children }) => {
             dispatch({ type: GET_PRODUCTS_ERROR });
         }
     };
+    const fetchSingleProduct = async (url) => {
+        dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+        try {
+            const response = await axios.get(url);
+            const products = response.data;
+            dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: products });
+        } catch (error) {
+            dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+        }
+    };
 
     useEffect(() => {
         fetchProducts(url);
     }, []);
     return (
         <ProductsContext.Provider
-            value={{ ...state, openSidebar, closeSidebar }}
+            value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
         >
             {children}
         </ProductsContext.Provider>
