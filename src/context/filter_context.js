e75cf286d8fab6e9df0244dc385904110ca6f105
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-14 08:34:09 +0300

    Sort UI and grid/list view buttons

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,7 @@ import { useProductsContext } from "./products_context";
 const initialState = {
     filtered_products: [],
     all_products: [],
-    grid_view: false,
+    grid_view: true,
 };
 
 const FilterContext = React.createContext();
@@ -27,8 +27,15 @@ export const FilterProvider = ({ children }) => {
     useEffect(() => {
         dispatch({ type: LOAD_PRODUCTS, payload: products });
     }, [products]);
+
+    const setGridView = () => {
+        dispatch({ type: SET_GRIDVIEW });
+    };
+    const setListView = () => {
+        dispatch({ type: SET_LISTVIEW });
+    };
     return (
-        <FilterContext.Provider value={{ ...state }}>
+        <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
             {children}
         </FilterContext.Provider>
     );
