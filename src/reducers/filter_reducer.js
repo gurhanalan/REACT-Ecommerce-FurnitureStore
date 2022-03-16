ffbfce6e9180affe33c5dbf8d2196614aec08154
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-16 16:45:05 +0300

    adding filter functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -96,8 +96,48 @@ const filter_reducer = (state, action) => {
         };
     }
     if (action.type === FILTER_PRODUCTS) {
-        // console.log("filtering products");
-        return { ...state };
+        const { all_products } = state;
+        const { text, category, company, color, price, shipping } =
+            state.filters;
+
+        let tempProducts = [...all_products];
+
+        // filtering
+        // text
+        if (text) {
+            tempProducts = tempProducts.filter((product) =>
+                product.name.toLowerCase().startsWith()
+            );
+        }
+        // category
+        if (category !== "all") {
+            tempProducts = tempProducts.filter(
+                (product) => product.category === category
+            );
+        }
+        // company
+        if (company !== "all") {
+            tempProducts = tempProducts.filter(
+                (product) => product.company === company
+            );
+        }
+        // colors
+        if (color !== "all") {
+            tempProducts = tempProducts.filter((product) =>
+                product.colors.find((c) => c === color)
+            );
+        }
+
+        // price
+        tempProducts = tempProducts.filter((product) => product.price <= price);
+        // shipping
+        if (shipping) {
+            tempProducts = tempProducts.filter(
+                (product) => product.shipping === true
+            );
+        }
+
+        return { ...state, filtered_products: tempProducts };
     }
     throw new Error(`No Matching "${action.type}" - action type`);
 };
