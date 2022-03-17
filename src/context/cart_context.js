a182cbea35b394921f84e14e9d960c8a6aaf6fe8
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-17 20:40:37 +0300

    Remove Item & Clear Cart functionalities

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -37,11 +37,15 @@ export const CartProvider = ({ children }) => {
         });
     };
     // remove item
-    const removeItem = () => {};
+    const removeItem = (id) => {
+        dispatch({ type: REMOVE_CART_ITEM, payload: id });
+    };
     // toggle amount
     const toggleAmount = (id, value) => {};
     // clear cart
-    const clearCart = () => {};
+    const clearCart = () => {
+        dispatch({ type: CLEAR_CART });
+    };
 
     useEffect(() => {
         localStorage.setItem("cart", JSON.stringify(state.cart));
