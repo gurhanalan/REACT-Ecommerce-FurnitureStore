ae35a435278d41bd8163075a9e165808739f30bf
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-21 13:34:05 +0300

    Update to react router dom 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 import {
     Home,
@@ -19,31 +19,22 @@ function App() {
             <Router>
                 <Navbar />
                 <Sidebar />
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
-                    <Route exact path="/cart">
-                        <Cart />
-                    </Route>
-                    <Route exact path="/products">
-                        <Products />
-                    </Route>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/products/:id" element={<SingleProduct />} />
                     <Route
-                        exact
-                        path="/products/:id"
-                        children={<SingleProduct />}
+                        path="/checkout"
+                        element={
+                            <PrivateRoute>
+                                <Checkout />
+                            </PrivateRoute>
+                        }
                     />
-                    <PrivateRoute exact path="/checkout">
-                        <Checkout />
-                    </PrivateRoute>
-                    <Route path="*">
-                        <Error />
-                    </Route>
-                </Switch>
+                    <Route path="*" element={<Error />} />
+                </Routes>
                 <Footer />
             </Router>
         </AuthWrapper>
